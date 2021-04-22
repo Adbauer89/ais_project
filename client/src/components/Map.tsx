326b96adf5a5a165a142b177a0c1b51ad94544dc
Alan Bauer <abauerdev@gmail.com>
2021-04-21 23:28:37 -0400

    Modify front end Map

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,22 +1,28 @@
 import React, {Fragment} from 'react';
 
-const Map = ({ currentImageName, handleClick }: {currentImageName: string, handleClick: any}) => {
+const Map = ({ currentImageId, handleClick }: {currentImageId: number, handleClick: any}) => {
 
     return (
         <Fragment>
             <div className={`map-image-container`} id={`map`} onClick={handleClick}>
-                { currentImageName &&
                     <svg viewBox="0 0 100 100" className="map-image"
-                         style={{backgroundImage: `url("./resources/${currentImageName}")`, backgroundSize: `cover`}}>
-                        <circle cx="12.5" cy="6.7" r="3" strokeWidth="1" stroke="#FF000055" fill="#FF000055">
+                         style={{backgroundImage: `url("http://localhost:3001/tile-image/${currentImageId}")`, backgroundSize: `cover`}}>
+                        <circle cx="25" cy="25" r="3" strokeWidth="1" stroke="#FF000055" fill="#FF000055">
+                            <animate attributeType="SVG" attributeName="r" begin="0s" dur="2.0s" repeatCount="indefinite" from="0%" to="1%"/>
+                            <animate attributeType="CSS" attributeName="stroke-width" begin="0s"  dur="2.0s" repeatCount="indefinite" from="0%" to="4%" />
+                            <animate attributeType="CSS" attributeName="opacity" begin="0s"  dur="2.0s" repeatCount="indefinite" from="1" to="0"/>
+                        </circle>
+                        <circle cx="25" cy="50" r="3" strokeWidth="1" stroke="#FF000055" fill="#FF000055">
+                            <animate attributeType="SVG" attributeName="r" begin="0s" dur="2.0s" repeatCount="indefinite" from="0%" to="1%"/>
+                            <animate attributeType="CSS" attributeName="stroke-width" begin="0s"  dur="2.0s" repeatCount="indefinite" from="0%" to="4%" />
+                            <animate attributeType="CSS" attributeName="opacity" begin="0s"  dur="2.0s" repeatCount="indefinite" from="1" to="0"/>
+                        </circle>
+                        <circle cx="25" cy="75" r="3" strokeWidth="1" stroke="#FF000055" fill="#FF000055">
                             <animate attributeType="SVG" attributeName="r" begin="0s" dur="2.0s" repeatCount="indefinite" from="0%" to="1%"/>
                             <animate attributeType="CSS" attributeName="stroke-width" begin="0s"  dur="2.0s" repeatCount="indefinite" from="0%" to="4%" />
                             <animate attributeType="CSS" attributeName="opacity" begin="0s"  dur="2.0s" repeatCount="indefinite" from="1" to="0"/>
                         </circle>
                     </svg>
-                }
-
-
             </div>
         </Fragment>
     )
--- client/src/components/MapContainer.tsx
+++ client/src/components/MapContainer.tsx
@@ -1,35 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Map from './Map';
 import SearchMenu from './SearchMenu';
 
 const MapContainer = () => {
     const [currentZoom, setCurrentZoom] = useState(1);
+    const [currentImageId, setCurrentImageId] = useState(1);
+    const [currentImageData, setCurrentImageData] = useState(
+        {   "id": 1,
+        "ICESName": "-1",
+        "west": 7.0,
+        "south": 54.5,
+        "east": 13.0,
+        "north": 57.5,
+        "scale": 1,
+        "filename": "ROOT.png",
+        "image_width": 2000,
+        "image_height": 2000,
+        "image_west": 7.0,
+        "image_south": 54.31614,
+        "image_east": 13.0,
+        "image_north": 57.669343,
+        "contained_by": -1
+        });
     const [zoomMode, setCurrentZoomMode] = useState('in');
     const [currentFocus, setCurrentFocus] = useState({
         longitude: 0,
         latitude: 0,
     });
-    const [images] = useState([
-        {   "id": 1,
-            "ICESName": "-1",
-            "west": 7.0,
-            "south": 54.5,
-            "east": 13.0,
-            "north": 57.5,
-            "scale": 1,
-            "filename": "ROOT.png",
-            "image_width": 2000, "image_height": 2000, "image_west": 7.0, "image_south": 54.31614, "image_east": 13.0, "image_north": 57.669343, "contained_by": -1},
-        {"id": 5330, "ICESName": "38G0", "west": 10.0, "south": 54.5, "east": 11.0, "north": 55.0, "scale": 2, "filename": "38G0.png", "image_width": 2000, "image_height": 2000, "image_west": 10.0, "image_south": 54.461175, "image_east": 11.0, "image_north": 55.038312, "contained_by": -1},
-        {"id": 53301, "ICESName": "38G01", "west": 10.0, "south": 54.75, "east": 10.5, "north": 55.0, "scale": 3, "filename": "38G01.png", "image_width": 2000, "image_height": 2000, "image_west": 10.0, "image_south": 54.731097, "image_east": 10.5, "image_north": 55.018777, "contained_by": 5330},
-        {"id": 53302, "ICESName": "38G02", "west": 10.5, "south": 54.75, "east": 11.0, "north": 55.0, "scale": 3, "filename": "38G02.png", "image_width": 2000, "image_height": 2000, "image_west": 10.5, "image_south": 54.731097, "image_east": 11.0, "image_north": 55.018777, "contained_by": 5330},
-        {"id": 53303, "ICESName": "38G03", "west": 10.0, "south": 54.5, "east": 10.5, "north": 54.75, "scale": 3, "filename": "38G03.png", "image_width": 2000, "image_height": 2000, "image_west": 10.0, "image_south": 54.480204, "image_east": 10.5, "image_north": 54.769665, "contained_by": 5330},
-        {"id": 53304, "ICESName": "38G04", "west": 10.5, "south": 54.5, "east": 11.0, "north": 54.75, "scale": 3, "filename": "38G04.png", "image_width": 2000, "image_height": 2000, "image_west": 10.5, "image_south": 54.480204, "image_east": 11.0, "image_north": 54.769665, "contained_by": 5330}]);
-    const [currentImage, setCurrentImage] = useState(images[0]);
 
     const handleClick = (e: { preventDefault: () => void; pageX: number; pageY: number;}) => {
         e.preventDefault();
-        console.log(calculateMapX(e) + ', ' + calculateMapY(e));
+
+        let newFocus = {
+            longitude: calculateMapX(e),
+            latitude: calculateMapY(e)
+        }
+
+        setCurrentFocus(newFocus);
+
         changeZoom();
     }
 
@@ -39,7 +48,7 @@ const MapContainer = () => {
         divLeft = document.getElementById('map')!.offsetLeft;
         divWidth = document.getElementById('map')!.offsetWidth;
 
-        return currentImage.west + (currentImage.east - currentImage.west) * ((e.pageX - divLeft) / divWidth);
+        return currentImageData.west + (currentImageData.east - currentImageData.west) * ((e.pageX - divLeft) / divWidth);
     }
 
     const calculateMapY = (e: any): number => {
@@ -48,7 +57,7 @@ const MapContainer = () => {
         divTop = document.getElementById('map')!.offsetTop;
         divHeight = document.getElementById('map')!.offsetHeight;
 
-        return currentImage.south + (currentImage.north - currentImage.south) * (1 - ((e.pageY - divTop) / divHeight));
+        return currentImageData.south + (currentImageData.north - currentImageData.south) * (1 - ((e.pageY - divTop) / divHeight));
     }
 
     const changeZoom = () => {
@@ -71,12 +80,10 @@ const MapContainer = () => {
         }
     }
 
-
-
     return (
         <section className={`map-container`}>
             <SearchMenu />
-            {currentImage.filename && <Map currentImageName={currentImage.filename} handleClick={handleClick}/> }
+            {currentImageData.filename && <Map currentImageId={currentImageId} handleClick={handleClick}/> }
         </section>
     )
 }
